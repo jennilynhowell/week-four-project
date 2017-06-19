6d0e5db30d4b2b30cc340698a354a9231d7fb47d
Jennilyn Howell <jennilynhowell@me.com>
2017-06-19 15:36:09 -0400

    fetch working but not printing to dom; branching to use JQ

--- js/main.js
+++ js/main.js
@@ -2,43 +2,42 @@
   'use strict';
 
   const urlTrack = 'https://api.soundcloud.com/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
-  const urlPlaylist = 'https://api.soundcloud.com/playlists?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
-
-  let trackSearchQuery = 'Hall of Fame';
-  let trackParams = '&q=' + trackSearchQuery;
-
-  let playlistSearchQuery = '';
-  let playlistParams = '&q=' + playlistSearchQuery
+  // const urlPlaylist = 'https://api.soundcloud.com/playlists?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
 
   let domHeader = document.querySelector('.page-header');
   let searchPlayDiv = document.querySelector('.search-play-div');
   let searchForm = document.querySelector('.track-search');
+  let searchInput = document.getElementById('searchInput');
   let trackPlay = document.querySelector('.track-play');
   let trackListDiv = document.querySelector('.track-list');
   let recommends = document.querySelector('.recommends');
 
-  searchForm.addEventListener('submit', function(e) {
+  //capture str entered in search field and perform fetch to get some song suggestions back
+
+  searchForm.addEventListener('submit', fetchSearch);
+
+  function fetchSearch (e){
     e.preventDefault();
-    alert("Submitted!")
-    let trackSearchEntry = e.target.value;
-    console.log(trackSearchEntry);
-  })
+    var searchParam = searchInput.value;
+    console.log(searchParam);
+    searchParam = '&q=' + searchParam;
 
+    fetch(urlTrack + searchParam).then(function(res) {
+      res.json().then(function(data){
+        data.forEach(function(i){
+          let title = i.title;
+          let artist = i.user.username;
+          let genre = i.genre;
+          let artwork = i.artwork_url;
+          console.log('TrackSearch: ', title, artist, genre, artwork);
+          nowPlaying(title, artist, genre, artwork);
+        });
 
-  // fetch(urlTrack).then(function(res) {
-  //   res.json().then(function(data){
-  //     data.forEach(function(i){
-  //       let title = i.title;
-  //       let artist = i.user.username;
-  //       let genre = i.genre;
-  //       let artwork = i.artwork_url;
-  //       console.log('TrackSearch: ', title, artist, genre, artwork);
-  //       nowPlaying(title, artist, genre, artwork);
-  //     });
-  //
-  //   })
-  //
-  // })
+      })
+
+    })
+  }
+
   //
   // fetch((urlPlaylist + playlistParams)).then(function(res) {
   //   res.json().then(function(data){
@@ -54,7 +53,7 @@
 function nowPlaying (title, artist, genre, artwork){
   let trackListData = document.createElement('ul');
   trackListData.setAttribute('class', 'list-inline list-unstyled');
-  trackListDiv.appendChild(trackListData);
+  trackListData.appendChild(trackListDiv);
 
   let artworkDiv = document.createElement('li');
   artworkDiv.setAttribute('id', 'artworkDiv');
@@ -65,7 +64,6 @@ function nowPlaying (title, artist, genre, artwork){
   let genreDiv = document.createElement('li');
   genreDiv.setAttribute('id', 'genreDiv');
 
-
   trackListData.appendChild(artworkDiv);
   trackListData.appendChild(titleDiv);
   trackListData.appendChild(artistDiv);
@@ -73,7 +71,10 @@ function nowPlaying (title, artist, genre, artwork){
 
   let artworkImg = document.createElement('div');
   artworkDiv.appendChild(artworkImg);
-  artworkImg.innerHTML = `<img src="${artwork}">`;
+  if (artwork) {
+    artworkImg.innerHTML = `<img src="${artwork}">`;
+  };
+
 
   let titleText = document.createElement('h4');
   titleDiv.appendChild(titleText);
@@ -89,8 +90,6 @@ function nowPlaying (title, artist, genre, artwork){
 
 
 }
-
-
 
 
 }());
