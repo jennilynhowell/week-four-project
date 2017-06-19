5a665e8ac9652c87b0aa2a7ebd238f5e9603bc46
Jennilyn Howell <jennilynhowell@me.com>
2017-06-19 17:33:47 -0400

    jq working + styled search results

--- js/main.js
+++ js/main.js
@@ -1,95 +1,55 @@
-(function(){
+(function($){
   'use strict';
 
   const urlTrack = 'https://api.soundcloud.com/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
-  // const urlPlaylist = 'https://api.soundcloud.com/playlists?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
 
-  let domHeader = document.querySelector('.page-header');
-  let searchPlayDiv = document.querySelector('.search-play-div');
-  let searchForm = document.querySelector('.track-search');
-  let searchInput = document.getElementById('searchInput');
-  let trackPlay = document.querySelector('.track-play');
-  let trackListDiv = document.querySelector('.track-list');
-  let recommends = document.querySelector('.recommends');
+  //JQuery ajax req and dom writing
+  let $searchResults = $('.search-results');
+  let title = '';
+  let artist = '';
+  let genre = '';
+  let artwork = '';
 
-  //capture str entered in search field and perform fetch to get some song suggestions back
-
-  searchForm.addEventListener('submit', fetchSearch);
-
-  function fetchSearch (e){
+  $('.form-inline').on('submit', function(e){
     e.preventDefault();
-    var searchParam = searchInput.value;
-    console.log(searchParam);
+    let searchParam = searchInput.value;
     searchParam = '&q=' + searchParam;
+    $.ajax(urlTrack + searchParam).then(function(data){
+      data.forEach(function(song){
+        let title = song.title;
+        let stream = song.stream_url;
+        let artist = song.user.username;
+        let genre = song.genre;
+        if (!genre){
+          genre = '(not provided)';
+        }
+        let artwork = song.artwork_url;
+        if (!artwork){
+          artwork = 'img/bird.jpg'
+        }
 
-    fetch(urlTrack + searchParam).then(function(res) {
-      res.json().then(function(data){
-        data.forEach(function(i){
-          let title = i.title;
-          let artist = i.user.username;
-          let genre = i.genre;
-          let artwork = i.artwork_url;
-          console.log('TrackSearch: ', title, artist, genre, artwork);
-          nowPlaying(title, artist, genre, artwork);
-        });
+        $searchResults.append(`<ul class="list-unstyled song-block">
+                                <li><img class="img-thumbnail" src="${artwork}"></li>
 
+                                <li id="stream-song"><a href="${stream}">Title: ${title}</a></li>
+
+                                <li>Artist: ${artist}</li>
+
+                                <li>Genre: ${genre}</li>
+
+                              </ul>
+                              `)
       })
+    });
 
-    })
-  }
+  let $streamSong = $('stream-song');
+  $streamSong.on('click', function(){
+    console.log($streamSong);
+    $('audio').attr('src', /*?????*/)
+  })
 
-  //
-  // fetch((urlPlaylist + playlistParams)).then(function(res) {
-  //   res.json().then(function(data){
-  //     data.forEach(function(data){
-  //       let title = data.title;
-  //       let artist = data.user.username;
-  //       let genre = data.genre;
-  //       // console.log('PlaylistSearch: ', title, artist, genre);
-  //     });
-  //   })
-  // })
-
-function nowPlaying (title, artist, genre, artwork){
-  let trackListData = document.createElement('ul');
-  trackListData.setAttribute('class', 'list-inline list-unstyled');
-  trackListData.appendChild(trackListDiv);
-
-  let artworkDiv = document.createElement('li');
-  artworkDiv.setAttribute('id', 'artworkDiv');
-  let titleDiv = document.createElement('li');
-  titleDiv.setAttribute('id', 'titleDiv');
-  let artistDiv = document.createElement('li');
-  artistDiv.setAttribute('id', 'artistDiv');
-  let genreDiv = document.createElement('li');
-  genreDiv.setAttribute('id', 'genreDiv');
-
-  trackListData.appendChild(artworkDiv);
-  trackListData.appendChild(titleDiv);
-  trackListData.appendChild(artistDiv);
-  trackListData.appendChild(genreDiv);
-
-  let artworkImg = document.createElement('div');
-  artworkDiv.appendChild(artworkImg);
-  if (artwork) {
-    artworkImg.innerHTML = `<img src="${artwork}">`;
-  };
+  })
 
 
-  let titleText = document.createElement('h4');
-  titleDiv.appendChild(titleText);
-  titleText.textContent = title;
 
-  let artistText = document.createElement('h5');
-  artistDiv.appendChild(artistText);
-  artistText.textContent = artist;
-
-  let genreText = document.createElement('p');
-  genreDiv.appendChild(genreText);
-  genreText.textContent = genre;
-
-
-}
-
-
-}());
+}(jQuery));
