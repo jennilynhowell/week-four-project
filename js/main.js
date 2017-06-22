7a1a9e2c8c3746b35077b0a229b4c428b16e04e3
Jennilyn Howell <jennilynhowell@me.com>
2017-06-21 22:29:26 -0400

    final functionality and styling

--- js/main.js
+++ js/main.js
@@ -21,15 +21,14 @@
           };
         let artwork = song.artwork_url;
           if (!artwork){
-            artwork = 'img/bird.jpg'
+            artwork = 'img/brown-bird.jpg'
           };
 
         //build dom
         $searchResults.append(`<ul class="list-unstyled song-block">
                                 <li><img class="img-thumbnail" src="${artwork}"></li>
-                                <li style="display: none" class="this-artwork"><p>'${artwork}'</p></li>
 
-                                <li id="stream-song"><a href="${stream}">Title: ${title}</a></li>
+                                <li><a class="stream-song" href="${stream}">Title: ${title}</a></li>
 
                                 <li>Artist: ${artist}</li>
 
@@ -38,19 +37,18 @@
                               </ul>
                               `)
 
-
         //play selected song and update headerArt div
         let $audio = $('audio');
-        let $headerImg = $('.album-art');
-        $searchResults.on('click', function(e){
-          e.preventDefault();
-          console.log(e.currentTarget);
-          //this is selecting the last 'artwork' b/c in a loop... how to change??????????????????????
-          // $headerImg.attr('src', artwork);
-          $headerImg.attr('alt', 'Album Artwork');
-          $audio.attr('src', stream);
+        let $songBlock = $('.song-block');
+        let $streamSong = $('.stream-song');
 
-        })
+        $songBlock.on('click', function(e){
+          e.preventDefault();
+          let $currentSong = e.target.href;
+
+          $audio.attr('src', $currentSong);
+
+        });
 
       })
     });
